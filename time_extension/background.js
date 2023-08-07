859f9d1451fb736c51f7e95b9c8197165d225583
OilyOogway <126415182+OilyOogway@users.noreply.github.com>
2023-08-07 15:30:02 -0500

    Fixed Flickering between break time and main time

--- time_extension/background.js
+++ time_extension/background.js
@@ -12,6 +12,7 @@ function formatTime(seconds) {
     .padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
 }
 
+
 chrome.alarms.onAlarm.addListener((alarm) => {
   chrome.storage.local.get(["timer", "isRunning"], (res) => {
     const time = res.timer ?? 0;
@@ -25,14 +26,14 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     chrome.action.setBadgeText({
       text: `${formatTime(time + 1)}`,
     });
-    chrome.storage.sync.get(["notificationTime"], (res) => {
-      const notificationTime = res.notificationTime ?? 1000;
-      if (time % notificationTime == 0) {
-        this.registration.showNotification("Flowmodoro Timer", {
-          body: `You have been focused for ${notificationTime} seconds!`,
-          icon: "koi_timer.png",
-        });
-      }
-    });
+    // chrome.storage.sync.get(["notificationTime"], (res) => {
+    //   const notificationTime = res.notificationTime ?? 1000;
+    //   if (time % notificationTime == 0) {
+    //     this.registration.showNotification("Flowmodoro Timer", {
+    //       body: `You have been focused for ${notificationTime} seconds!`,
+    //       icon: "koi_timer.png",
+    //     });
+    //   }
+    // });
   });
 });
--- time_extension/popup.js
+++ time_extension/popup.js
@@ -1,7 +1,13 @@
+//grabs the h2 elements in the popup.html
 const timeElement = document.getElementById("time");
 const nameElement = document.getElementById("name");
 const timerElement = document.getElementById("timer");
 
+const startStopBtn = document.getElementById("start-stop");
+const resetBtn = document.getElementById("reset");
+const breakBtn = document.getElementById("break");
+
+//formats time by inputting seconds and working with it returns a string
 function formatTime(seconds) {
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
@@ -12,6 +18,8 @@ function formatTime(seconds) {
     .padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
 }
 
+//retrieves time from google storage then displays it onto the timer h2
+//element
 function updateTimeElements() {
   chrome.storage.local.get(["timer"], (res) => {
     const time = res.timer ?? 0;
@@ -20,18 +28,55 @@ function updateTimeElements() {
   });
 }
 
-updateTimeElements();
-setInterval(updateTimeElements, 1000);
+let interval; // Interval for main timer display
+let breakInterval; // Interval for break countdown
+let isBreakCountdownActive = false; // Flag to track break countdown status
 
-// chrome.storage.sync.get(["name"], (res) => {
-//   const name = res.name ?? "???";
-//   nameElement.textContent = `Your name is: ${name}`;
-// });
+function startMainInterval() {
+  interval = setInterval(updateTimeElements, 1000);
+}
 
-const startStopBtn = document.getElementById("start-stop");
-//const stopBtn = document.getElementById("stop");
-const resetBtn = document.getElementById("reset");
-const breakBtn = document.getElementById("break");
+startMainInterval();
+
+// updateTimeElements();
+// setInterval(updateTimeElements, 1000);
+
+breakBtn.addEventListener("click", () => {
+    chrome.storage.local.set({
+      isRunning: false,
+    });
+  
+    clearInterval(interval); // Clear the main timer interval
+  
+    // Grab the current time from the timer and divide it by 5
+    chrome.storage.local.get(["timer"], (res) => {
+      const currentTime = res.timer ?? 0;
+      const breakTime = Math.floor(currentTime / 5);
+  
+      // Set the break countdown time
+      breakCountdown = breakTime;
+      isBreakCountdownActive = true; // Set the flag to true
+  
+      breakInterval = setInterval(() => {
+        if (breakCountdown > 0) {
+          breakCountdown--;
+        //   breakTimerElement.style.display = "block"; // Show the break countdown element
+          timerElement.textContent = `Break Time: ${formatTime(breakCountdown)}`;
+        } else {
+          clearInterval(breakInterval);
+          timerElement.textContent = "Break is over!";
+          chrome.storage.local.set({
+            timer: 0,
+            isRunning: false,
+          });
+  
+          isBreakCountdownActive = false; // Set the flag to false
+          startMainInterval(); // Restart the main timer interval
+        }
+      }, 1000);
+    });
+  });
+
 
 startStopBtn.addEventListener("click", () => {
   chrome.storage.local.get(["isRunning"], (res) => {
@@ -63,34 +108,34 @@ let countdownInterval;
 let breakCountdown; // Add a variable to track the countdown time
 
 // Event listener for the "Break" button
-breakBtn.addEventListener("click", () => {
-  chrome.storage.local.set({
-    isRunning: false,
-  });
+// breakBtn.addEventListener("click", () => {
+//   chrome.storage.local.set({
+//     isRunning: false,
+//   });
 
-  //clearInterval(countdownInterval); // Clear the ongoing timer interval
+//   //clearInterval(countdownInterval); // Clear the ongoing timer interval
 
-  // Grab the current time from the timer and divide it by 5
-  chrome.storage.local.get(["timer"], (res) => {
-    const currentTime = res.timer ?? 0;
-    const breakTime = Math.floor(currentTime / 5);
+//   // Grab the current time from the timer and divide it by 5
+//   chrome.storage.local.get(["timer"], (res) => {
+//     const currentTime = res.timer ?? 0;
+//     const breakTime = Math.floor(currentTime / 5);
 
-    // Set the break countdown time
-    breakCountdown = breakTime;
+//     // Set the break countdown time
+//     breakCountdown = breakTime;
 
-    // Start counting down the break time
-    countdownInterval = setInterval(() => {
-      if (breakCountdown > 0) {
-        breakCountdown--;
-        timerElement.textContent = `Break Time: ${formatTime(breakCountdown)}`;
-      } else {
-        clearInterval(countdownInterval);
-        timerElement.textContent = "Break is over!";
-        chrome.storage.local.set({
-          timer: 0,
-          isRunning: false,
-        });
-      }
-    }, 1000);
-  });
-});
+//     // Start counting down the break time
+//     countdownInterval = setInterval(() => {
+//       if (breakCountdown > 0) {
+//         breakCountdown--;
+//         timerElement.textContent = `Break Time: ${formatTime(breakCountdown)}`;
+//       } else {
+//         clearInterval(countdownInterval);
+//         timerElement.textContent = "Break is over!";
+//         chrome.storage.local.set({
+//           timer: 0,
+//           isRunning: false,
+//         });
+//       }
+//     }, 1000);
+//   });
+// });
