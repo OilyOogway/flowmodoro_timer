826efd3828b7fd12c8c2d9f3b631aed9b8824e92
OilyOogway <126415182+OilyOogway@users.noreply.github.com>
2023-08-07 07:46:10 -0700

    Add files via upload

--- time_extension/background.js
+++ time_extension/background.js
@@ -2,6 +2,16 @@ chrome.alarms.create({
   periodInMinutes: 1 / 60,
 });
 
+function formatTime(seconds) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const secs = seconds % 60;
+
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
+}
+
 chrome.alarms.onAlarm.addListener((alarm) => {
   chrome.storage.local.get(["timer", "isRunning"], (res) => {
     const time = res.timer ?? 0;
@@ -13,7 +23,7 @@ chrome.alarms.onAlarm.addListener((alarm) => {
       timer: time + 1,
     });
     chrome.action.setBadgeText({
-      text: `${time + 1}`,
+      text: `${formatTime(time + 1)}`,
     });
     chrome.storage.sync.get(["notificationTime"], (res) => {
       const notificationTime = res.notificationTime ?? 1000;
--- time_extension/popup.js
+++ time_extension/popup.js
@@ -16,7 +16,7 @@ function updateTimeElements() {
   chrome.storage.local.get(["timer"], (res) => {
     const time = res.timer ?? 0;
     const formattedTime = formatTime(time);
-    timerElement.textContent = `The timer is at: ${formattedTime}`;
+    timerElement.textContent = `${formattedTime}`;
   });
 }
 
@@ -28,19 +28,27 @@ setInterval(updateTimeElements, 1000);
 //   nameElement.textContent = `Your name is: ${name}`;
 // });
 
-const startBtn = document.getElementById("start");
-const stopBtn = document.getElementById("stop");
+const startStopBtn = document.getElementById("start-stop");
+//const stopBtn = document.getElementById("stop");
 const resetBtn = document.getElementById("reset");
+const breakBtn = document.getElementById("break");
 
-startBtn.addEventListener("click", () => {
-  chrome.storage.local.set({
-    isRunning: true,
-  });
-});
+startStopBtn.addEventListener("click", () => {
+  chrome.storage.local.get(["isRunning"], (res) => {
+    const isRunning = res.isRunning ?? false;
 
-stopBtn.addEventListener("click", () => {
-  chrome.storage.local.set({
-    isRunning: false,
+    // Toggle the value of isRunning
+    const newIsRunning = !isRunning;
+
+    chrome.storage.local.set({
+      isRunning: newIsRunning,
+    });
+
+    if (newIsRunning) {
+      startStopBtn.textContent = "Stop Timer";
+    } else {
+      startStopBtn.textContent = "Start Timer";
+    }
   });
 });
 
@@ -48,5 +56,41 @@ resetBtn.addEventListener("click", () => {
   chrome.storage.local.set({
     timer: 0,
     isRunning: false,
+  });
+});
+
+let countdownInterval;
+let breakCountdown; // Add a variable to track the countdown time
+
+// Event listener for the "Break" button
+breakBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: false,
+  });
+
+  //clearInterval(countdownInterval); // Clear the ongoing timer interval
+
+  // Grab the current time from the timer and divide it by 5
+  chrome.storage.local.get(["timer"], (res) => {
+    const currentTime = res.timer ?? 0;
+    const breakTime = Math.floor(currentTime / 5);
+
+    // Set the break countdown time
+    breakCountdown = breakTime;
+
+    // Start counting down the break time
+    countdownInterval = setInterval(() => {
+      if (breakCountdown > 0) {
+        breakCountdown--;
+        timerElement.textContent = `Break Time: ${formatTime(breakCountdown)}`;
+      } else {
+        clearInterval(countdownInterval);
+        timerElement.textContent = "Break is over!";
+        chrome.storage.local.set({
+          timer: 0,
+          isRunning: false,
+        });
+      }
+    }, 1000);
   });
 });
